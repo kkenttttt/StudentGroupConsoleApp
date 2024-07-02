cc3a186ce6137d2d204bb101b3796c6e5e9d1ad0
Елизавета <lizalizapotter123@gmail.com>
2024-07-03 00:26:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Group;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var group = new StudentGroup("AAAA", "BBBB");
+        group.AddStudent(new Student("John Doe", 3.50));
+        group.AddStudent(new Student("Jane Doe", 4.00));
+        group.AddStudent(new Student("Jim Smith", 3.00));
+        group.ShowAll();
+        Console.ReadKey();
+    }
+}
